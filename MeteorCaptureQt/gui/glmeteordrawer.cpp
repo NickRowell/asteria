20f552daf8f1e6a3e39be28137ff2fe698d1e96a
Nick Rowell <nr@roe.ac.uk>
2017-02-04 14:03:57 +0000

    Adding V4L stuff

---
+++ MeteorCaptureQt/gui/glmeteordrawer.cpp
@@ -0,0 +1,6 @@
+#include "glmeteordrawer.h"
+
+GLMeteorDrawer::GLMeteorDrawer()
+{
+
+}
---
+++ MeteorCaptureQt/gui/glmeteordrawer.h
@@ -0,0 +1,11 @@
+#ifndef GLMETEORDRAWER_H
+#define GLMETEORDRAWER_H
+
+
+class GLMeteorDrawer
+{
+public:
+    GLMeteorDrawer();
+};
+
+#endif // GLMETEORDRAWER_H
