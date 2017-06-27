fbb51d7e517557266cd33a1617d29ea70a66874e
NickRowell <nick_rowell@hotmail.com>
2017-06-27 18:11:56 +0100

    Delete logging.cpp

--- Asteria/infra/logging.cpp
+++
@@ -1,8 +0,0 @@
-#include "infra/logging.h"
-
-#include <stdio.h>
-
-QTextStream Logging::stdOutStream(stdout);
-QTextStream Logging::stdErr(stderr);
-QFile Logging::logFile("asteria.log");
-QTextStream Logging::logFileOut(&Logging::logFile);
